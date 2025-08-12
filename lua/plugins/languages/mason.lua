caa6d222b8b40d9561eab1427dc10fdf2476764d
Deniel <strummerdeniel@gmail.com>
2025-08-12 11:50:31 +0300

    Fixing the issue of lsp message appearing twice

--- lua/plugins/languages/mason.lua
+++ lua/plugins/languages/mason.lua
@@ -1,52 +1,52 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"clang-format",
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup(opts)
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-			ensure_installed = {
-				"pyright",
-				"lua_ls",
-				"ts_ls",
-				"clangd",
-				"jsonls",
-				"marksman",
-				"gopls",
-				"html",
-				"cssls",
-			},
-		},
-		config = function(_, opts)
-			require("mason-lspconfig").setup(opts)
-		end,
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		opts = {
-			ensure_installed = {
-				"codelldb",
-				"python",
-				"delve",
-				"js",
-				"node2",
-			},
-			handlers = {
-				function(config)
-					require("mason-nvim-dap").default_setup(config)
-				end,
-			},
-		},
-	},
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "clang-format",
+            },
+        },
+        config = function(_, opts)
+            require("mason").setup(opts)
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+            ensure_installed = {
+                "pyright",
+                "lua_ls",
+                "ts_ls",
+                "clangd",
+                "jsonls",
+                "marksman",
+                "gopls",
+                "html",
+                "cssls",
+            },
+        },
+        config = function(_, opts)
+            require("mason-lspconfig").setup(opts)
+        end,
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        opts = {
+            ensure_installed = {
+                "codelldb",
+                "python",
+                "delve",
+                "js",
+                "node2",
+            },
+            handlers = {
+                function(config)
+                    require("mason-nvim-dap").default_setup(config)
+                end,
+            },
+        },
+    },
 }
