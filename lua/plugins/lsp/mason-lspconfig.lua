bd961e59d4f58a06f092474ee7ee30283e922636
spartancmd <strummerdeniel@gmail.com>
2026-08-05 17:13:29 +0300

    init

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,15 +1,25 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	dependencies = {
-		"mason-org/mason.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	opts = {
-		-- NOTE: possible, but better to do it explicitly in lspconfig.lua
-		-- automatic_enable = true,
-		ensure_installed = {
-			"lua_ls",
-			"pyright",
-		},
-	},
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+        "mason-org/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    opts = {
+        -- NOTE: possible, but better to do it explicitly in lspconfig.lua
+        -- automatic_enable = true,
+        ensure_installed = {
+            "lua_ls",
+            "pyright",
+            "jdtls",
+            "ts_ls",
+            "html",
+            "cssls",
+            "jsonls",
+            "yamlls",
+            "dockerls",
+            "bashls",
+            "marksman",
+            "taplo",
+        },
+    },
 }
