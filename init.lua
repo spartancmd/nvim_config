4acc4c038db310f832b155f0dc34f4a2a3e75cdc
spartancmd <strummerdeniel@gmail.com>
2026-08-06 14:56:19 +0300

    add: vscode setup for neovim extension

--- init.lua
+++ init.lua
@@ -1,1 +1,19 @@
+if vim.g.vscode then
+    vim.g.mapleader = " "
+    vim.g.maplocalleader = ","
+    vim.opt.clipboard = "unnamedplus"
+
+    vim.keymap.set("n", "<leader>cn", function()
+        vim.fn.VSCodeNotify("editor.action.rename")
+    end)
+    vim.keymap.set("n", "K", function()
+        vim.fn.VSCodeNotify("editor.action.showHover")
+    end)
+    vim.keymap.set("n", "<leader>cf", function()
+        vim.fn.VSCodeNotify("editor.action.formatDocument")
+    end)
+
+    return
+end
+
 require("core")
